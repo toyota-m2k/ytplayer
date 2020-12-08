274c17dc7186934f233ec308a719295aec824b87
toyota-m2k <toyota.mitsuki@gmail.com>
2020-12-09 01:07:42 +0900

    dbまわり

---
+++ ytplayer/data/DLEntry.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ytplayer.data {
+    public enum Status {
+        INIT,
+        WAITING,
+        DOWNLOADING,
+        DOWNLOADED,
+        FAILED,
+        CANCELLED,
+        DELETED,
+    }
+    public enum Rating {
+        DREADFUL,
+        BAD,
+        NORMAL,
+        GOOD,
+        EXCELLENT,
+    }
+
+    [Table(Name = "t_download")]
+    public class DLEntry {
+        [Column(Name = "id", IsPrimaryKey = true, IsDbGenerated =true)]
+        int id;
+        [Column(Name = "url", CanBeNull = false)]
+        string url;
+        [Column(Name = "name", CanBeNull = false)]
+        string name;
+        [Column(Name = "path", CanBeNull = false)]
+        string path;
+        [Column(Name = "status", CanBeNull = false)]
+        int status;
+        [Column(Name = "rating", CanBeNull = false)]
+        int rating;
+        [Column(Name = "category", CanBeNull = true)]
+        string category;
+        [Column(Name = "date", CanBeNull = false)]
+        int date;
+        [Column(Name = "desc", CanBeNull = true)]
+        string desc;
+
+        public Rating Rating {
+            get => (Rating)rating;
+            set => rating = (int)value;
+        }
+        public Status Status {
+            get => (Status)status;
+            set => status = (int)value;
+        }
+
+        public DLEntry(string url) {
+            id = 0;
+            this.url = url;
+            name = "";
+            path = "";
+            Status = Status.INIT;
+            Rating = Rating.NORMAL;
+            category = "-";
+        }
+    }
+}
--- ytplayer/data/Storage.cs
+++ ytplayer/data/Storage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq;
 using System.Data.SQLite;
 using System.Linq;
 using System.Text;
@@ -36,19 +37,27 @@ namespace ytplayer.data {
         }
         private void InitTables() {
             executeSql(
-                @"CREATE TABLE IF NOT EXISTS t_downloaded (
+                @"CREATE TABLE IF NOT EXISTS t_download (
                     id INTEGER NOT NULL PRIMARY KEY,
                     url TEXT NOT NULL UNIQUE,
                     name TEXT NOT NULL,
                     path TEXT NOT NULL,
                     status INTEGER NOT NULL,
-                    desc TEXT,
-                    flags INTEGER DEFAULT '0',
+                    rating INTEGER DEFAULT '0',
                     date INTEGER DEFAULT '0'
+                    category TEXT,
+                    desc TEXT,
                 )",
                 @"CREATE INDEX IF NOT EXISTS idx_date ON t_downloaded(path)"
             );
         }
 
+        public void Add(DLEntry entry) {
+            using (var context = new DataContext(Connection)) {
+                var table = context.GetTable<DLEntry>();
+                table.InsertOnSubmit(entry);
+                context.SubmitChanges();
+            }
+        }
     }
 }
